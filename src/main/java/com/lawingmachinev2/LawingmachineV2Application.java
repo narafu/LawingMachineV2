4300d4248c841cceec3ddbc21a1f8cd24505ab42
narafu <67089503+narafu@users.noreply.github.com>
2022-08-07 15:38:26 +0900

    git ignore commit

---
+++ src/main/java/com/lawingmachinev2/LawingmachineV2Application.java
@@ -0,0 +1,13 @@
+package com.lawingmachinev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawingmachineV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawingmachineV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lawingmachinev2/LawingmachineV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawingmachinev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawingmachineV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
